0a9cbbdda67ed7e4207285e7aadbf2ab9f6b2f45
Teguh Arief <114749743+teguharifudin@users.noreply.github.com>
2024-07-08 15:34:54 +0700

    added Horoscope function

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -8,6 +8,56 @@ import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { AuthGuard } from '../auth/auth.guard';
 
+function dateToHoroscopeSign(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  return getHoroscopeSign(day, month);
+}
+
+function getHoroscopeSign(day, month) {
+  var horoscopeSigns = {
+    'capricorn':'Capricorn',
+    'aquarius':'Aquarius',
+    'pisces':'Pisces',
+    'aries':'Aries',
+    'taurus':'Taurus',
+    'gemini':'Gemini',
+    'cancer':'Cancer',
+    'leo':'Leo',
+    'virgo':'Virgo',
+    'libra':'Libra',
+    'scorpio':'Scorpio',
+    'sagittarius':'Sagittarius'
+  }
+
+  if((month == 1 && day <= 20) || (month == 12 && day >=22)) {
+    return horoscopeSigns.capricorn;
+  } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+    return horoscopeSigns.aquarius;
+  } else if((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+    return horoscopeSigns.pisces;
+  } else if((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+    return horoscopeSigns.aries;
+  } else if((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+    return horoscopeSigns.taurus;
+  } else if((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+    return horoscopeSigns.gemini;
+  } else if((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+    return horoscopeSigns.cancer;
+  } else if((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+    return horoscopeSigns.leo;
+  } else if((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+    return horoscopeSigns.virgo;
+  } else if((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+    return horoscopeSigns.libra;
+  } else if((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+    return horoscopeSigns.scorpio;
+  } else if((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+    return horoscopeSigns.sagittarius;
+  }
+}
+
 @Controller()
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
@@ -30,6 +80,7 @@ export class ProfileController {
   create(@Body() createProfileDto: CreateProfileDto, @UploadedFile() file, @Req() req: any) {
     createProfileDto.file = file.filename
     createProfileDto.username = req.user.username
+    createProfileDto.horoscope = dateToHoroscopeSign(req.body.birthday)
     return this.profileService.create(createProfileDto);
   }
 
